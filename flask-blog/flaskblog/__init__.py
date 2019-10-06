8e0516331b53c9f20a27c6ed6d78edf0a8775d5d
Rafael Lima <rafael.lima@univem.edu.br>
2019-10-06 19:45:43 -0300

    blueprints

--- flask-blog/flaskblog/__init__.py
+++ flask-blog/flaskblog/__init__.py
@@ -2,14 +2,31 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ffc9b08361e211247f1ab1fb6978f902'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+mail = Mail()
 
-from flaskblog import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
